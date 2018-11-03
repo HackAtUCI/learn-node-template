5e53d6f616a9b561c43c27d53164f6e8a3b7d72e
danhn255 <danhn255@gmail.com>
2018-11-02 18:34:11 -0700

    Final Template

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const adapter = new FileSync('db.json');
 const db = low(adapter);
 let id = null;
 
+console.log("All Dependencies Properly Installed");
+
 function initializeDatabase()
 {
     db.defaults({chores:{},count:0}).write();
@@ -56,25 +58,48 @@ app.get("/background.jpg",function (req,res)
 
 //TODO -----------------------------------------------
 
+/*
+// TODO 1: Setup the endpoint for serving the website.
+// Note: You can send any content that is pass back via res.write() or res.end()
 app.get('/',function (req,res) {
+    // TODO 2: Serve the html webpage
     res.end(fs.readFileSync("app.html"));
 });
+*/
 
-app.post("/addChore",bodyParser.json(),function (req,res) { 
+/*
+//TODO 3: Setup the endpoint for adding new chores
+//Note: We are using the POST method because we expect data to be sent with the body of the request
+//Note: bodyParser is a middleware that convert JSON string send with the body to an object that we can work with.
+app.post("/addChore",bodyParser.json(),function (req,res) {
+    //TODO 4: Adding data 
+    //Explain the method of adding to the database
     addToDatabase(req.body);
+    //TODO 5: End the request with a success message
     res.end("success");
 })
+*/	
 
+
+
+/*
+//TODO 6: Add endpoint for getting all the chores from the database
 app.get("/getChores",function (req,res) {
+    //Explain the getAllChoresFromDatabase() method
     res.end(JSON.stringify(getAllChoresFromDatabase()));
 })
+*/
 
+/*
+
+//TODO 7: Add endpoint for deleting a chore when it is completed
+//Note: The data being passed will be part of the url which is called a query
 app.get("/deleteChore",function(req,res) {
+    //Explain the deleteFromDatabase() method
     deleteFromDatabase(req.query.cardID);
     res.end("success");
 })
-
-
+*/
 
 
 
