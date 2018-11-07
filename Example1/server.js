dab0ddf9d47a91add92ddc82ddb62f94d87d8c2f
danhn255 <danhn255@gmail.com>
2018-11-06 17:36:37 -0800

    Changed Demo Final

--- Example1/server.js
+++ Example1/server.js
@@ -1,22 +1,35 @@
 const http = require("http");
-
 const hostname = '127.0.0.1';
 const port = 3000;
+const fs = require("fs");
+const path = require("path");
+const helloWorldPath = path.resolve("./Example1/HelloWorld.html");
 
 //Place your name here
 const name = "Bob"
 
-function main(req,res)
-{
-    if (req.url == "/home" && req.method == "GET"){
+
+
+
+//Routing Demo
+/*
+    if ((req.url == "/" || req.url == "/home") && req.method == "GET"){
         console.log("home page served");
-        res.end("Hello world");
+        console.log(helloWorldPath);
+        res.end(fs.readFileSync(helloWorldPath));
     }
     else if (req.url == "/getName"&& req.method == "GET")
     {   
+        console.log("Name Served");
         res.setHeader("Access-Control-Allow-Origin","*");;
         res.end(name);
     }
+*/
+
+
+function main(req,res)
+{
+
 }
 
 const server=http.createServer(main);
